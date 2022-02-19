c27e56acc957e4aa59f53a1bacf9b947671d2e90
Bert Wagner <bertwagner@bertwagner.com>
2022-02-19 07:55:16 -0500

    start variablalizing downloading with xword-dl

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A locally hosted website for easily downloadomg crossword files and sending them to a network printer
 
+## Background
+
+I like solving crosswords on paper. I want to be able to print puzzles from a variety of source easily.  This web app runs on a Raspberry Pi server on my local network and allows me to print puzzles on-demand via my phone.
+
 ## Instructions
 1. Run `FLASK_APP=app/dashboard.py flask run --host=0.0.0.0 --port=80`. 
 2. Open [http://localhost:5000](http://localhost:5000) in your browser.
--- app/dashboard.py
+++ app/dashboard.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, request
 import subprocess
+import os
 
 #MOVE TO CONFIG
 PRINTER_NAME='BrotherHL2170W'
@@ -15,20 +16,39 @@ def index():
 @app.route('/print', methods=['POST'])
 def print():
 
-    # Download the puzzle (check if already in cache)
+    publisher = request.form['publisher']
+    date = request.form['date']
+
+    download_puzzle(publisher,date)
 
     send_to_printer(PRINTER_NAME,'crossword.pdf')
 
     return redirect('/')
 
+def download_puzzle(publisher,date):
+    # Check cache before downloading
+
+    folder_path_pdf = f'../data/pdf/{publisher}/'
+    folder_path_puz = f'../data/puz/{publisher}/'
+
+    # TODO get this to save in the right spot
+    cmd = f'cd ../data/puz/{publisher}/ && xword-dl {publisher} --output {date}.puz'
+    process = subprocess.Popen(cmd.split())
+    output, error = process.communicate()
 
 def send_to_printer(printer_name,file_name):
     cmd = f'lp -n 1 -o fit-to-page -d {printer_name} {file_name}'
     
     try:
-        #process = subprocess.Popen(cmd.split())
-        #output, error = process.communicate()
+        process = subprocess.Popen(cmd.split())
+        output, error = process.communicate()
         flash("Your file has been sent to the printer","success")
     except Exception as e:
         flash(f'Something went wrong with printing: {str(e)}',"error")
 
+
+def __make_folder_if_not_exists(folder_path,delete_files):
+    path_exists = os.path.exists(folder_path)
+
+    if not path_exists:
+        os.makedirs(folder_path)
