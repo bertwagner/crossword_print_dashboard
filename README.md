553664e62c47d9a86bfdbce415379f1bf838d1eb
Bert Wagner <bertwagner@bertwagner.com>
2022-02-18 20:29:10 -0500

    add basic print functionality

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Crossword Print Dashboard
 
-A locally hosted website for easily downloadomg crossword puz files and sending them to a network printer
+A locally hosted website for easily downloadomg crossword files and sending them to a network printer
 
 ## Instructions
 1. Run `FLASK_APP=app/dashboard.py flask run --host=0.0.0.0 --port=80`. 
--- app/dashboard.py
+++ app/dashboard.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, redirect, flash
+import subprocess
+
+#MOVE TO CONFIG
+PRINTER_NAME='BrotherHL2170W'
+APP_SECRET='not-so-secret'
 
 app = Flask(__name__)
-app.secret_key='not-so-secret'
+app.secret_key=APP_SECRET
 
 @app.route('/')
 def index():
@@ -12,7 +17,17 @@ def print():
 
     # Download the puzzle (check if already in cache)
 
-    # Send to printer
+    send_to_printer(PRINTER_NAME,'crossword.pdf')
 
-    flash("Your file has been sent to the printer")
+    flash("Your file has been sent to the printer","success")
     return redirect('/')
+
+
+def send_to_printer(printer_name,file_name):
+    cmd = f'lp -n 1 -o fit-to-page -d {printer_name} {file_name}'
+    process = subprocess.Popen(cmd.split())
+    output, error = process.communicate()
+
+    # TODO: TEST THIS, FLASH ERROR MESSAGES ON HOME PAGE
+    if error != None:
+        flash('Something went wrong with printing: {error}',"error")
